0564da8b9f4aa64e849500c1ce7b1f19da07802b
zarasah <zarinasah1989@gmail.com>
2023-02-16 00:08:10 +0400

    add router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import  { Routes, Route } from 'react-router-dom'
+import Header from './Header';
+import Main from './Main';
+import About from './About'
+import Sidebar from './Sidebar';
+import Footer from './Footer';
 import './App.css';
 
 export default function App() {
-  return 
+  return (
+    <div className = "container">
+    <Header />
+    <div className = "wrapper">
+      <Sidebar />
+      <Routes>
+        <Route path = "/" element = {<Main />}/>
+        <Route path = "/about" element = {<About />}/>
+        <Route />
+      </Routes>
+    </div>
+    <Footer />
+    </div>
+  );
+  // return (
+  //   <div className = "container">
+  //     <Header />
+  //     <div className = "wrapper">
+  //       <Main />
+  //       <Sidebar />
+  //     </div>
+  //     <Footer />
+  //   </div>
+  // )
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,7 @@
+export default function Sidebar() {
+    return (
+        <div className = "sidebar">
+            <nav>Sidebar</nav>
+        </div>
+    );
+}
