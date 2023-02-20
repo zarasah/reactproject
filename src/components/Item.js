d71927e635916f11557d5b510cf367c4cfe255d8
zarasah <zarinasah1989@gmail.com>
2023-02-20 11:38:04 +0400

    add ADDBUTTON

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,32 @@
+import { useState } from "react";
+
 export default function Item(props) {
     const { name, price, img, id } = props.data;
+    const [isActive, setActive] = useState(false);
+
+    function handleMouseOver() {
+        setActive(true);
+    }
+
+    function handleMouseOut() {
+        setActive(false);
+    }
+
+    function click() {
+
+    }
 
     return (
-        <div className = "item">
-            <a href = {id}><img src = {img} alt = {name} className = "item-img"/></a>
-            <h4>{name}</h4>
-            <p>$ {price} USD</p>
+        <div className = "item" onMouseOver={handleMouseOver} onMouseOut = {handleMouseOut}>
+            <div>
+                <a href = {id}><img src = {img} alt = {name} className = "item-img"/></a>
+            </div>
+            <div className = "discrip">
+                <h4>{name}</h4>
+                <p>$ {price} USD</p>
+                <input type = "submit" value = "ADD TO CART" className = {isActive ? 'add-btn-active' : 'add-btn'} onClick = {click}/>
+            </div>
         </div>
     )
 }
+
