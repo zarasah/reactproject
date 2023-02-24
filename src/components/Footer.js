30aa004263923cf98445a0af35c0f7c53021d61f
zarasah <zarinasah1989@gmail.com>
2023-02-24 10:17:32 +0400

    change a tag to Link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'react-router-dom';
 
 import './Footer.css';
 
@@ -11,27 +12,27 @@ export default function Footer() {
             <div className = "footer-left">
                 <h5>Pages</h5>
                 <ul>
-                    <li><a href = "/">Home</a></li>
-                    <li><a href = "/blog">Blog</a></li>
-                    <li><a href = "/about">About</a></li>
-                    <li><a href = "/contact">Contact</a></li>
+                    <li><Link to = "/">Home</Link></li>
+                    <li><Link to = "/blog">Blog</Link></li>
+                    <li><Link to = "/about">About</Link></li>
+                    <li><Link to = "/contact">Contact</Link></li>
                 </ul>
             </div>
             <div className = "footer-center">
                 <h5>Shop</h5>
                 <ul>
-                    <li><a href = "/#">All products</a></li>
-                    <li><a href = "/#">Accessories</a></li>
-                    <li><a href = "/#">Clothes</a></li>
-                    <li><a href = "/#">Perfume</a></li>
-                    <li><a href = "/#">Shoes</a></li>
+                    <li><Link to = "/#">All products</Link></li>
+                    <li><Link to = "/#">Accessories</Link></li>
+                    <li><Link to = "/#">Clothes</Link></li>
+                    <li><Link to = "/#">Perfume</Link></li>
+                    <li><Link to = "/#">Shoes</Link></li>
                 </ul>
             </div>
             <div className = "footer-right">
                 <ul className = "brands">
-                    <li><a href = "https://www.facebook.com" target = "_blank" rel='noreferrer'><FontAwesomeIcon icon={faFacebook} className = "icon"/></a></li>
-                    <li><a href = "https://www.instagram.com/" target = "_blank" rel='noreferrer'><FontAwesomeIcon icon={faInstagram} className = "icon"/></a></li>
-                    <li><a href = "https://twitter.com/?lang=ru" target = "_blank" rel='noreferrer'><FontAwesomeIcon icon={faTwitter} className = "icon"/></a></li>
+                    <li><Link to = "https://www.facebook.com" target = "_blank" rel='noreferrer'><FontAwesomeIcon icon={faFacebook} className = "icon"/></Link></li>
+                    <li><Link to = "https://www.instagram.com/" target = "_blank" rel='noreferrer'><FontAwesomeIcon icon={faInstagram} className = "icon"/></Link></li>
+                    <li><Link to = "https://twitter.com/?lang=ru" target = "_blank" rel='noreferrer'><FontAwesomeIcon icon={faTwitter} className = "icon"/></Link></li>
                 </ul>
             </div>
         </div>
